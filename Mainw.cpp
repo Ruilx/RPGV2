a2ab6fe1a93af0c4fb20d51d4d9cfb5c7d86fef3
Ruilx <799382072@qq.com>
2017-01-02 03:42:38 +0800

    Mainw debug update
    Ruilx

--- Mainw.cpp
+++ Mainw.cpp
@@ -5,20 +5,6 @@ Mainw::Mainw(QWidget *parent)
 {
 	this->setCentralWidget(this->w);
 	this->setFixedSize(642, 482);
-
-	QImage *a = new QImage(100, 100, QImage::Format_RGBA8888_Premultiplied);
-	a->fill(Qt::transparent);
-	QPainter p(a);
-	p.setCompositionMode(QPainter::CompositionMode_Source);
-	p.setPen(QColor(Qt::black));
-	p.drawLine(0, 0, 50, 50);
-	a->save("1.png");
-	p.drawLine(0, 0, 75, 75);
-	a->save("2.png");
-	p.drawLine(0, 0, 100, 100);
-	a->save("3.png");
-	p.end();
-	a->save("4.png");
 }
 
 Mainw::~Mainw()
