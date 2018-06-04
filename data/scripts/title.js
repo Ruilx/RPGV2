e28b5833c7deca19b52ff435f48d3c5bc54393ec
Ruilx <799382072@qq.com>
2018-06-04 11:18:06 +0800

    ready to combine

---
+++ data/scripts/title.js
@@ -0,0 +1,20 @@
+// title.js
+function init(){
+	RpgBanner.setBackgroundColor("#000000");
+	RpgBanner.setForegroundPixmap("data/images/background/title2.png");
+	RpgBanner.setStartOpacity(0);
+	RpgBanner.setEndOpacity(1);
+	RpgBanner.exec();
+	RpgBanner.waitingForBannerComplete();
+	RpgUtils.msleep(2000);
+	RpgBanner.setStartOpacity(1);
+	RpgBanner.setEndOpacity(0);
+	RpgBanner.execExit();
+	RpgBanner.waitingForBannerComplete();
+	RpgUtils.msleep(2000);
+
+	RpgMusic.playMusic("op1");
+	return "exit";
+}
+
+init();
